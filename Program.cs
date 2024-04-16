de3ad2334d962486feafb7c9f794ebb7f537c58b
JaydenMeekoly <st10306957@vcconnect.edu.za>
2024-04-16 19:44:05 +0200

    i have created the menu that will display for the user

--- Program.cs
+++ Program.cs
@@ -13,6 +13,28 @@ namespace RecipeApp
     {
         static void Main(string[] args)
         {
+            Recipe recipe = new Recipe();
+            bool exit = false;
+
+            //start of the while loop---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            while (!exit)
+            {
+                Console.WriteLine("Recipe App");
+                Console.WriteLine("1. Enter Recipe Details");
+                Console.WriteLine("2. Display Recipe");
+                Console.WriteLine("3. Scale Recipe");
+                Console.WriteLine("4. Reset Quantities");
+                Console.WriteLine("5. Clear All Data");
+                Console.WriteLine("6. Exit");
+                Console.Write("Enter your choice: ");
+
+                int choice;
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    continue;
+                }
+
+            }
         }
-    }
 }
